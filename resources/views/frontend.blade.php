a04d7aafc92f48e689cab139ad1574f34711a2cc
cindy-fatika <cfatika17@gmai.com>
2025-05-21 22:21:34 +0700

    PR Pertemuan 11

---
+++ resources/views/frontend.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Frontend 5026231039</title>
+</head>
+<body>
+    <h1>Daftar Semua Halaman Pertemuan 1 - ETS</h1>
+    <ul>
+        <li><a href="{{ url('/boostrap1') }}">boostrap1.blade.php</a></li>
+        <li><a href="{{ url('/bostrap2') }}">bostrap2.blade.php</a></li>
+        <li><a href="{{ url('/font') }}">font.blade.php</a></li>
+        <li><a href="{{ url('/fontfallbacks') }}">fontfallbacks.blade.php</a></li>
+        <li><a href="{{ url('/fontfamily') }}">fontfamily.blade.php</a></li>
+        <li><a href="{{ url('/fontshorthand') }}">fontshorthand.blade.php</a></li>
+        <li><a href="{{ url('/fontwebsafe') }}">fontwebsafe.blade.php</a></li>
+        <li><a href="{{ url('/formets') }}">formets.blade.php</a></li>
+        <li><a href="{{ url('/googlefont') }}">googlefont.blade.php</a></li>
+        <li><a href="{{ url('/Greatfontpairings') }}">Greatfontpairings.blade.php</a></li>
+        <li><a href="{{ url('/indexets') }}">indexets.blade.php</a></li>
+        <li><a href="{{ url('/js1') }}">js1.blade.php</a></li>
+        <li><a href="{{ url('/objectposition') }}">objectposition.blade.php</a></li>
+        <li><a href="{{ url('/pertama') }}">pertama.blade.php</a></li>
+        <li><a href="{{ url('/template1') }}">template1.blade.php</a></li>
+        <li><a href="{{ url('/tugasbootstrap1') }}">tugasbootstrap1.blade.php</a></li>
+        <li><a href="{{ url('/tugasbootstrap2') }}">tugasbootstrap2.blade.php</a></li>
+        <li><a href="{{ url('/tugaslayout') }}">tugaslayout.blade.php</a></li>
+        <li><a href="{{ url('/tugaslinktree') }}">tugaslinktree.blade.php</a></li>
+        <li><a href="{{ url('/tugaslogin') }}">tugaslogin.blade.php</a></li>
+        <li><a href="{{ url('/tugaslayout') }}">tugaslayout.blade.php</a></li>
+        <li><a href="{{ url('/tutorial') }}">tutorial.blade.php</a></li>
+        <li><a href="{{ url('/validasi1') }}">validasi1.blade.php</a></li>
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 // pembahasan java: import java.io (use: di php)
 
 //pembahasan java: System.out.println("hello world");
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -26,4 +24,99 @@ Route::get('halo', function () {
 
 Route::get('blog', function () {
 	return view('blog');
+});
+
+
+
+Route::get('/frontend', function () {
+    return view('frontend');
+});
+
+
+Route::get('/boostrap1', function () {
+    return view('boostrap1');
+});
+
+Route::get('/bostrap2', function () {
+    return view('bostrap2');
+});
+
+Route::get('/font', function () {
+    return view('font');
+});
+
+Route::get('/fontfallbacks', function () {
+    return view('fontfallbacks');
+});
+
+Route::get('/fontfamily', function () {
+    return view('fontfamily');
+});
+
+Route::get('/fontshorthand', function () {
+    return view('fontshorthand');
+});
+
+Route::get('/fontwebsafe', function () {
+    return view('fontwebsafe');
+});
+
+Route::get('/formets', function () {
+    return view('formets');
+});
+
+Route::get('/googlefont', function () {
+    return view('googlefont');
+});
+
+Route::get('/Greatfontpairings', function () {
+    return view('Greatfontpairings');
+});
+
+Route::get('/indexets', function () {
+    return view('indexets');
+});
+
+Route::get('/js1', function () {
+    return view('js1');
+});
+
+Route::get('/objectposition', function () {
+    return view('objectposition');
+});
+
+Route::get('/pertama', function () {
+    return view('pertama');
+});
+
+Route::get('/template1', function () {
+    return view('template1');
+});
+
+Route::get('/tugasbootstrap1', function () {
+    return view('tugasbootstrap1');
+});
+
+Route::get('/tugasbootstrap2', function () {
+    return view('tugasbootstrap2');
+});
+
+Route::get('/tugaslayout', function () {
+    return view('tugaslayout');
+});
+
+Route::get('/tugaslinktree', function () {
+    return view('tugaslinktree');
+});
+
+Route::get('/tugaslogin', function () {
+    return view('tugaslogin');
+});
+
+Route::get('/tutorial', function () {
+    return view('tutorial');
+});
+
+Route::get('/validasi1', function () {
+    return view('validasi1');
 });
