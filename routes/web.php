22cb683f710f7eac5cc8e6db4d842d3692074104
cindy-fatika <cfatika17@gmai.com>
2025-06-05 16:06:31 +0700

    Pertemuan 14

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
+
 
 // pembahasan java: import java.io (use: di php)
 Route::get('/', function () {
@@ -14,7 +16,7 @@ Route::get('/', function () {
 Route::get('dosen',[DosenController::class,'index']);
 Route::get('welcome',[DosenController::class,'welcome']);
 
-Route::get('/pegawai/{nama}', [PegawaiController::class,'index']);
+//Route::get('/pegawai/{nama}', [PegawaiController::class,'index']);
 Route::get('/formulir', [PegawaiController::class,'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class,'proses']);
 
@@ -127,3 +129,13 @@ Route::get('/tutorial', function () {
 Route::get('/validasi1', function () {
     return view('validasi1');
 });
+
+//route pegawaiDB
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']); //jika form dikirim, route ini akan dijalankan
+Route::get('/pegawai/edit/{id}',[PegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update',[PegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
+
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);
